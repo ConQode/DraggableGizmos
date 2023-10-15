ea0dada8f2d1fc3a3f15c313ddb4d5931cec0dd7
ConQode <42714530+ConQode@users.noreply.github.com>
2023-10-15 15:41:03 +0200

    Add files via upload

---
+++ DraggableGizmos/DraggableGizmo.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using IllusionPlugin;
+using DraggableGizmos;
+
+public class DraggableGizmo : MonoBehaviour
+{
+    private bool isDragging = false;
+    private Vector3 offset;
+    private Vector3 initialPosition;
+
+    void OnDrawGizmos()
+    {
+        if (isDragging)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawSphere(transform.position, 0.2f);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        initialPosition = transform.position;
+    }
+
+    void OnMouseUp()
+    {
+        isDragging = false;
+    }
+
+    void Update()
+    {
+        if (isDragging)
+        {
+            Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            transform.position = newPosition;
+        }
+    }
+}
